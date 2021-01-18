3fec0e1e69d729969bf0fdb2a6d5ef79a2099151
MRtotchi <bigbird19980311@gmail.com>
2021-01-18 16:52:08 +0900

    一時的にメールの送信先を自分宛に変換

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'kandai.algorithm@gmail'
   layout 'mailer'
 end
--- app/mailers/purchase_order_mailer.rb
+++ app/mailers/purchase_order_mailer.rb
@@ -4,7 +4,7 @@ class PurchaseOrderMailer < ApplicationMailer
       @user = user
       mail(
         from: 'kandai.algorithm@gmail.com',
-        to:   'ebara@kansai-u.ac.jp',
+        to:   'bigbird19980311@gmail.com',
         subject: '書籍の注文依頼fromアルゴリズム研究室',
       )
     end
