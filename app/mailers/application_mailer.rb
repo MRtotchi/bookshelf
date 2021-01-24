ad96dd4c252c9255b1e568236e4ea1a9802719c5
MRtotchi <bigbird19980311@gmail.com>
2021-01-24 11:56:46 +0900

    gamilアプリパスワードの再設定

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'kandai.algorithm@gmail'
+  default from: 'kandai.algorithm@gmail.com'
   layout 'mailer'
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,8 +59,8 @@ config.action_mailer.smtp_settings = {
  address:              'smtp.gmail.com',
  port:                  587,
  domain:               'gmail.com',
- user_name:            'kandai.algo@gmail.com',
- password:             'udnxoyxuzqmwmkpe',
+ user_name:            Rails.application.credentials.gmail[:address],
+ password:             Rails.application.credentials.gmail[:password],
  authentication:       'plain',
  enable_starttls_auto:  true
 }
