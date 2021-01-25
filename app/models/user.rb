c8241ea563c7cbd992978c0e06c858e7bda39ae2
MRtotchi <bigbird19980311@gmail.com>
2021-01-25 11:19:11 +0900

    annotateのインストールとメール送信ボタンの文面の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  name                   :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  group_id               :bigint
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_group_id              (group_id)
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (group_id => groups.id)
+#
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -7,6 +32,8 @@ class User < ApplicationRecord
   has_many :books
   has_many :rental_books, class_name: "Book", foreign_key: :rental_user_id
   belongs_to :group
+  has_many :rentals
+  has_many :books, through: :rentals
   
   
 end
